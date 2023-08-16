58a2f217c0bda55cdfe28c2cdc9b374b31fd4ab9
ryan-g13 <ryan.groesch@gmail.com>
2023-08-16 14:39:23 -0700

    Update / refactor for tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,73 +1,54 @@
 import { useState } from 'react';
 import './App.css';
-import Square from './Square';
+import Board from './Board';
 
 function App() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
   }
 
-  function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
     } else {
-      nextSquares[i] = "O";
+      description = 'Go to game start';
     }
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
-  }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="App">
-      <h1>Tic Tac Toe</h1>
-      <div className='status'>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)}/>
-        <Square value={squares[1]}  onClick={() => handleClick(1)}/>
-        <Square value={squares[2]}  onClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]}  onClick={() => handleClick(3)}/>
-        <Square value={squares[4]}  onClick={() => handleClick(4)}/>
-        <Square value={squares[5]}  onClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]}  onClick={() => handleClick(6)}/>
-        <Square value={squares[7]}  onClick={() => handleClick(7)}/>
-        <Square value={squares[8]}  onClick={() => handleClick(8)}/>
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <div className="game-info">
+        <ol>{moves}</ol>
       </div>
     </div>
   );
 }
+
+/*
+TODOS: 
+For the current move only, show “You are at move #…” instead of a button.
+Rewrite Board to use two loops to make the squares instead of hardcoding them.
+Add a toggle button that lets you sort the moves in either ascending or descending order.
+When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+Display the location for each move in the format (row, col) in the move history list.
+*/
 
 export default App;
